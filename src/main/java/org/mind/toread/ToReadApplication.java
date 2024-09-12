105423aa815730e793a74bec12b9007684bc7a8e
sue-23 <85052088+sue-23@users.noreply.github.com>
2024-09-12 16:51:23 +0900

    Initial commit: Add README and .gitignore

---
+++ src/main/java/org/mind/toread/ToReadApplication.java
@@ -0,0 +1,14 @@
+package org.mind.toread;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToReadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToReadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mind/toread/ToReadApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mind.toread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToReadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
